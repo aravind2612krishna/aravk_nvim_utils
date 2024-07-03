2e29b482616c73637bd1ed116022c7fe91798dc4
aravk <aravk@altair.com>
2024-07-03 11:07:55 +0530

    [WIP] Testing addition of termdebug and smartcodecopy

--- lua/aravk_nvim_utils/init.lua
+++ lua/aravk_nvim_utils/init.lua
@@ -1,7 +1,14 @@
-local function example()
-  return true
-end
+-- Lazy load everything into utils.
+local aravk_nvim_utils = setmetatable({}, {
+  __index = function(t, k)
+    local ok, val = pcall(require, string.format('aravk_nvim_utils.%s', k))
 
-return {
-  example = example,
-}
+    if ok then
+      rawset(t, k, val)
+    end
+
+    return val
+  end,
+})
+
+return aravk_nvim_utils
---
+++ lua/aravk_nvim_utils/termdebug/init.lua
@@ -0,0 +1,59 @@
+local termdebugutils = {}
+
+function termdebugutils.setupdbg()
+  vim.cmd.packadd('termdebug')
+  vim.g.termdebug_wide = 1
+  vim.cmd('Termdebug')
+  vim.cmd('wincmd k')
+  vim.api.nvim_win_close(0, false)
+  vim.cmd('wincmd H')
+  vim.cmd('wincmd l')
+
+  if not vim.g['termdebug_config'] then
+    vim.g.termdebug_config = {}
+  end
+  vim.g.termdebug_config['map_K'] = 0
+
+  vim.cmd('hi debugPC term=reverse ctermbg=darkgrey guibg=darkblue')
+  vim.cmd('hi debugBreakpoint term=reverse ctermbg=blue guibg=red')
+
+  vim.keymap.set('n', '<leader>gu', function()
+    local lno = vim.fn.line('.')
+    local dbgcmd = 'until ' .. lno
+    vim.fn.TermDebugSendCommand(dbgcmd)
+  end, { desc = 'Gdb run until' })
+  vim.keymap.set('n', '<leader>jb', function()
+    local lno = vim.fn.line('.')
+    vim.fn.TermDebugSendCommand('tbreak ' .. lno)
+    vim.fn.TermDebugSendCommand('jump ' .. lno)
+  end, { desc = 'Gdb jump back' })
+  vim.keymap.set('n', '<leader>gc', function()
+    vim.cmd('Continue')
+  end, { desc = 'Continue' })
+  vim.keymap.set('n', '<CR>', '<cmd>Over<CR>', { desc = 'Step over' })
+  vim.keymap.set('n', '<leader>gk', function()
+    vim.fn.TermDebugSendCommand('up')
+  end, { desc = 'Up' })
+  vim.keymap.set('n', '<leader>gj', function()
+    vim.fn.TermDebugSendCommand('down')
+  end, { desc = 'Down' })
+  vim.keymap.set('n', '<leader>]', '<cmd>Step<CR>', { desc = 'Step into' })
+  vim.keymap.set('n', '<leader>[', '<cmd>Finish<CR>', { desc = 'Step out of' })
+  --"<cmd>call TermDebugSendCommand('p ' . expand("<cword>"))<CR>")
+  vim.keymap.set('n', '<leader>k', function()
+    local dbgcmd = 'p ' .. vim.fn.expand('<cexpr>')
+    vim.fn.TermDebugSendCommand(dbgcmd)
+  end, { desc = 'Gdb print under cursor' })
+  vim.keymap.set({ 'n', 'v' }, '\\', function()
+    vim.cmd('Evaluate')
+  end, { desc = 'Evaluate' })
+  vim.keymap.set('n', '<leader>ga', function()
+    vim.fn.TermDebugSendCommand('pattach hwx')
+  end, { desc = 'attach to recent hwx' })
+  vim.keymap.set('n', '<leader>gb', '<cmd>Break<CR>', { desc = 'Add a breakpoint' })
+  vim.keymap.set('n', '<leader>ge', function()
+    vim.fn.TermDebugSendCommand('extractStarCommandsFromLastOutput')
+  end, { desc = 'Run extractStarCommandsFromLastOutput' })
+end
+
+return termdebugutils
